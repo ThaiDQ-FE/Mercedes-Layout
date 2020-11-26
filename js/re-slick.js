94de05088f1a389cae29d41b38a57855ae33e9dc
ThaiChubby <dangquocthai07061998@gmail.com>
2020-11-26 18:41:49 +0700

    re-slick

--- js/re-slick.js
+++ js/re-slick.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
-    $('.navbar__slick').slick({
+    $('.header__slick').slick({
       dots: true,
       slidesToShow: 1,
       infinite: false,
       centerPadding: 0,
       slidesPerRow: 0,
-      autoplay: true,
-      autoplaySpeed: 2000,
       nextArrow: '<i class="fa fa-chevron-right"></i>',
       prevArrow: '<i class="fa fa-chevron-left"></i>',
       customPaging: function(slider, i) { 
         return '<div class="tab">' + '</div>';
     },
+  });
+});
+
+
+
+
+
+  $(document).ready(function(){
+    $('.carousel__slick').slick({
+      dots: false,
+      slidesToShow: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      nextArrow: '<i class="fa fa-chevron-right carousel-right"></i>',
+      prevArrow: '<i class="fa fa-chevron-left carousel-left"></i>',
     });
   });
 
-  $(document).ready(function(){
-    $('.navbar__slick2').slick({
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        centerPadding: 0,
-        slidesPerRow: 0,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: '<i class="fa fa-chevron-right"></i>',
-        prevArrow: '<i class="fa fa-chevron-left"></i>',
-        customPaging: function(slider, i) { 
-          return '<div class="tab">' + '</div>';
-        },
-    });
-  });
+  
